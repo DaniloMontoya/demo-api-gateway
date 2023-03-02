e748cb81849986b821972e131b028766d2d5295a
DaniloMontoya <danilo9831montoya@gmail.com>
2023-03-02 11:31:40 -0500

    - initial commit for demo spring api gateway

---
+++ src/test/java/com/devcodes/apigateway/UcoApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcodes.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcoApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
